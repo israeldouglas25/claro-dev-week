584fd47f1c0b17ee26a59aaa2dc2a9a280151edf
Israel Douglas <75404480+israeldouglas25@users.noreply.github.com>
2024-10-17 17:22:26 -0300

    Initial commit

---
+++ src/main/java/me/dio/claro_dev_week/ClaroDevWeekApplication.java
@@ -0,0 +1,13 @@
+package me.dio.claro_dev_week;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaroDevWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaroDevWeekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/claro_dev_week/ClaroDevWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.claro_dev_week;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaroDevWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
